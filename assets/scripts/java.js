a043da1da07a2a300244b4dd0f5ddd7064fb5757
Brannon Laveder <blaveder1@gmail.com>
2018-06-07 21:16:17 -0600

    train

--- assets/scripts/java.js
+++ assets/scripts/java.js
@@ -18,12 +18,11 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Initial Values
-var trainName = "";
-var role = "";
-var email = "";
+var name = "";
+var destination = "";
+var time = "";
 var rate = "";
-var firstTime = "";
-var tFrequency = "";
+var tMinutesTillTrain = "";
 
 //TIME
 
@@ -33,25 +32,38 @@ $("#add-user").on("click", function (event) {
 
     // Grabbed values from text boxes
     name = $("#name-input").val().trim();
-    role = $("#role-input").val().trim();
-    firstTime = $("#date-input").val().trim();
-    tFrequency = $("#rate-input").val().trim();
+    destination = $("#role-input").val().trim();
+    time = $("#date-input").val().trim();
+    rate = $("#rate-input").val().trim();
 
+    $(".form-control").val("");
 
     // Code for handling the push
     database.ref().push({
         name: name,
-        role: role,
-        // dateAdded: firebase.database.ServerValue.TIMESTAMP,
-        firstTime: firstTime,
-        tFrequency: tFrequency
+        destination: destination,
+        time: time,
+        rate: rate,
+        tMinutesTillTrain: tMinutesTillTrain
     });
     // adding to table
-    console.log(firstTime);
-    console.log(tFrequency);
+
+});
+
+// Firebase watcher .on("child_added"
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+    var sv = childSnapshot.val();
+    var newName = (sv.name);
+    var newDestination = (sv.destination);
+    var time = (sv.time);
+    var newRate = (sv.rate);
+
+    // Time is...
+    // var firstTime = "05:30";
 
     // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var firstTimeConverted = moment(time, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
     // Current Time
@@ -63,59 +75,33 @@ $("#add-user").on("click", function (event) {
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
+    var tRemainder = diffTime % newRate;
     console.log(tRemainder);
 
     // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
+    var tMinutesTillTrain = newRate - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-});
+    console.log(nextTrain);
+    var trainArrival = (moment(nextTrain).format("hh:mm"));
 
-// Firebase watcher .on("child_added"
-database.ref().on("child_added", function (snapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = snapshot.val();
-    getInfo();
-    // Console.loging the last user's data
-    // console.log(sv.name);
-    // console.log(sv.role);
-    // console.log(sv.rate);
-    // console.log(sv.dateAdded);
+    console.log(newName);
+    console.log(newDestination);
+    console.log(nextTrain);
+    console.log(newRate);
+
+    $("#data-holder").append("<tr><td>" + newName + "</td><td>" + newDestination + "</td><td>" + newRate + "</td><td>" + trainArrival + "</td><td>"
+        + tMinutesTillTrain + "</td></tr>");
 
 
-    // Change the HTML to reflect
-    // $("#name-display").text(sv.name);
-    // $("#email-display").text(sv.email);
-    // $("#age-display").text(sv.age);
-    // $("#comment-display").text(sv.comment);
-
-    function getInfo() {
-        var employeeTable = $("#trains");
-        var newRow = $("<tr>");
-        var newName = $("<td>");
-        var newRole = $("<td>");
-        var newDate = $("<td>");
-        var newRate = $("<td>");
-
-
-
-        employeeTable.append(newRow);
-        newRow.append(newName);
-        newRow.append(newRole);
-        newRow.append(newDate);
-        newRow.append(newRate);
-
-        newName.text(sv.name);
-        newRole.text(sv.role);
-        newDate.text(sv.firstTime);
-        newRate.text(sv.tFrequency);
-    }
-    // Handle the errors
 
 }, function (errorObject) {
+
     console.log("Errors handled: " + errorObject.code);
+});
+
+$("#reset").on("click", function () {
+    $("td").empty();
 });
